b360607524de3b25307d854217faea4f655e6a9b
zainkhan1726 <160050990+zainkhan1726@users.noreply.github.com>
2024-09-26 19:00:35 +0500

    fixed

--- script.ts
+++ script.ts
@@ -1,7 +1,8 @@
-"use strict";
-const skillSection = document.getElementById('skills');
-const toggleButton = document.getElementById('toggle-skills');
+const skillSection = document.getElementById('skills')!;
+const toggleButton = document.getElementById('toggle-skills')!;
+
 toggleButton.addEventListener('click', () => {
     skillSection.classList.toggle('hidden');
 });
+
 skillSection.classList.add('hidden');
